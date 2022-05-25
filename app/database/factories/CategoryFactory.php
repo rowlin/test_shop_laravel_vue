e8101ef659775b2ae1242ccc4de423e3f5bbf44a
alice <rowlinest90@gmail.com>
2022-05-25 12:46:58 +0300

    fix Factories

--- app/database/factories/CategoryFactory.php
+++ app/database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-           'name' => $this->faker->words(2)
+           'name' => $this->faker->unique(['name'])->word
         ];
     }
 }
---
+++ app/database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique('name')->text(100),
+            'text' => $this->faker->text(300),
+            'user_id' => function(){
+                return User::first()->id;
+            }
+        ];
+    }
+}
