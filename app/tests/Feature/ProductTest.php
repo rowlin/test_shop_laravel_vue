5df89ed6aea553e692b0ea3cc34209f0d94e0c58
alice <rowlinest90@gmail.com>
2022-05-25 12:50:12 +0300

    add Post model

---
+++ app/tests/Feature/ProductTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_GetProducts()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+}
